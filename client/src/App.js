59be494d4de25d9db10958bee92a8ae801d26cf1
Aryan <aryan4p130202@gmail.com>
2023-02-15 03:01:35 +0530

    in memory cache and mutations added

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,34 @@ import Header from "./components/Header";
 import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+/*
+This is done to get rid of the warning: Cache Data may be lost when 
+replacing the clients field of a Query object. So we defined an InMemoryCache
+to merge the existing and incoming data.
+*/
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+    fields: {
+      clients: {
+        merge(existing, incoming) {
+          return incoming;
+        },
+      },
+      projects: {
+        merge(existing, incoming) {
+          return incoming;
+        },
+      },
+    },
+  },
+  },
+});
+
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/ClientMutations";
+import { GET_CLIENTS } from "../queries/ClientQueries";
 
+/*
+    If we use only this then we can delete the client but the client 
+    will still be there in the table. We need to update the cache or refresh
+    the page to see the changes. To do that we need to use the useMutation hook
+
+    But to handle this we can refresh the query (this can be costly during multiple queries call) 
+    or update the cache.
+
+*/
 const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    }
+  });
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
