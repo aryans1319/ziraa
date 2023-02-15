03a5bb7c85e2aeaecc39fb3c720afe6ba82f98b1
Aryan <aryan4p130202@gmail.com>
2023-02-16 01:55:39 +0530

    bug fixes

--- server/index.js
+++ server/index.js
@@ -12,12 +12,6 @@ const app = express();
 app.use(cors());
 connectDB();
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.use(
   '/graphql',
   graphqlHTTP({
@@ -25,6 +19,13 @@ app.use(
     graphiql: process.env.NODE_ENV === 'development',
   })
 );
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
